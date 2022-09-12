0f49ecb26dbbb774c23ad5cc8749d9b9d93a958a
abdulhelmi1 <102794110+abdulhelmi1@users.noreply.github.com>
2022-09-12 23:50:30 +0700

    soal 4 prak 2 ya sayyyy

---
+++ praktikum2/soal4prak2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+/*program menampilkan deret bilagan genap dari 1-20  */
+public class soal4prak2
+{
+  public static void main(String args[]){
+    System.out.println("Deret Bilangan Genap");
+        for(int i=1;i<=20;i++){
+            if (i%2==0)
+            System.out.print(i+" ");
+    
+        }
+  }
+}
